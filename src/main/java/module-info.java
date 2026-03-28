e5b63951e2582be3103eb63cf06183bad9f00c66
Agon.Hoxhaj <agon.hoxhaj@student.uni-pr.edu>
2026-03-28 10:27:44 +0100

    added mysql dependency, and java.sql package

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module org.example.fiekconsultationscheduler {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
 
 
     opens org.example.fiekconsultationscheduler to javafx.fxml;
