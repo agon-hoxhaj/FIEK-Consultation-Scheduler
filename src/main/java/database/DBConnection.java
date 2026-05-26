fa91207d9e6fe4d6cce2613628bbd9c1fa69b2c5
Agon.Hoxhaj <agon.hoxhaj@student.uni-pr.edu>
2026-05-27 00:40:54 +0300

    database connection

---
+++ src/main/java/database/DBConnection.java
@@ -0,0 +1,23 @@
+package database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DBConnection {
+    private static final String DB_URL = "jdbc:postgresql://localhost/knk_projekti";
+    private static final String USER = "postgres";
+    private static final String PASSWORD = "1234";
+    private static Connection connection;
+    public static Connection getConnection() {
+        try {
+            if (connection == null) {
+                connection = DriverManager.getConnection(DB_URL, USER, PASSWORD);
+            }
+            return connection;
+
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
